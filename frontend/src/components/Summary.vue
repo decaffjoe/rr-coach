aca6f903b50f24feb4d1ed9340eae85e1e6d6592
decaffjoe <jw3mail+decaff@pm.me>
2020-06-28 17:00:57 -0500

    Component generalization refactor

--- frontend/src/components/Summary.vue
+++ frontend/src/components/Summary.vue
@@ -8,87 +8,17 @@
         <button @click="getPreviousWorkoutSummary" v-if="this.$cookies.isKey('user_id')">Get Summary</button>
         <div>
             <table>
-                <tr><th colspan="2">Pullups</th></tr>
-                <tr id="legend">
-                    <td>Progression</td>
-                    <td>Reps</td>
-                </tr>
-                <tr v-for="set of pullups" :key="set.id">
-                    <td>{{ pullupProgression[set.progression].name }}</td>
-                    <td>{{ set.reps }}</td>
-                </tr>
-                <tr><th colspan="2">Squats</th></tr>
-                <tr id="legend">
-                    <td>Progression</td>
-                    <td>Reps</td>
-                </tr>
-                <tr v-for="set of squats" :key="set.id">
-                    <td>{{ squatProgression[set.progression].name }}</td>
-                    <td>{{ set.reps }}</td>
-                </tr>
-                <tr><th colspan="2">Dips</th></tr>
-                <tr id="legend">
-                    <td>Progression</td>
-                    <td>Reps</td>
-                </tr>
-                <tr v-for="set of dips" :key="set.id">
-                    <td>{{ dipProgression[set.progression].name }}</td>
-                    <td>{{ set.reps }}</td>
-                </tr>
-                <tr><th colspan="2">Hinges</th></tr>
-                <tr id="legend">
-                    <td>Progression</td>
-                    <td>Reps</td>
-                </tr>
-                <tr v-for="set of hinges" :key="set.id">
-                    <td>{{ hingeProgression[set.progression].name }}</td>
-                    <td>{{ set.reps }}</td>
-                </tr>
-                <tr><th colspan="2">Rows</th></tr>
-                <tr id="legend">
-                    <td>Progression</td>
-                    <td>Reps</td>
-                </tr>
-                <tr v-for="set of rows" :key="set.id">
-                    <td>{{ rowProgression[set.progression].name }}</td>
-                    <td>{{ set.reps }}</td>
-                </tr>
-                <tr><th colspan="2">Pushups</th></tr>
-                <tr id="legend">
-                    <td>Progression</td>
-                    <td>Reps</td>
-                </tr>
-                <tr v-for="set of pushups" :key="set.id">
-                    <td>{{ pushupProgression[set.progression].name }}</td>
-                    <td>{{ set.reps }}</td>
-                </tr>
-                <tr><th colspan="2">Core: Anti-Extensions</th></tr>
-                <tr id="legend">
-                    <td>Progression</td>
-                    <td>Reps</td>
-                </tr>
-                <tr v-for="set of antiExtensions" :key="set.id">
-                    <td>{{ antiExtensionProgression[set.progression].name }}</td>
-                    <td>{{ set.reps }}</td>
-                </tr>
-                <tr><th colspan="2">Core: Anti-Rotations</th></tr>
-                <tr id="legend">
-                    <td>Progression</td>
-                    <td>Reps</td>
-                </tr>
-                <tr v-for="set of antiRotations" :key="set.id">
-                    <td>{{ antiRotationProgression[set.progression].name }}</td>
-                    <td>{{ set.reps }}</td>
-                </tr>
-                <tr><th colspan="2">Core: Extensions</th></tr>
-                <tr id="legend">
-                    <td>Progression</td>
-                    <td>Reps</td>
-                </tr>
-                <tr v-for="set of extensions" :key="set.id">
-                    <td>{{ extensionProgression[set.progression].name }}</td>
-                    <td>{{ set.reps }}</td>
-                </tr>
+                <div v-for="ex in Object.keys(sets)" :key="ex">
+                    <tr><th colspan="2">{{ `${ex[0].toUpperCase()}${ex.slice(1)}s` }}</th></tr>
+                    <tr id="legend">
+                        <td>Progression</td>
+                        <td>Reps</td>
+                    </tr>
+                    <tr v-for="set of sets[ex]" :key="set.id">
+                        <td>{{ progressions[`${ex}Progression`][set.progression].name }}</td>
+                        <td>{{ set.reps }}</td>
+                    </tr>
+                </div>
             </table>
         </div>
     </div>
@@ -124,41 +54,12 @@ export default {
         },
     },
     computed: {
-        pullups() {
-            if (this.summary['pullup']) return this.summary['pullup'];
-            else return [];
-        },
-        squats() {
-            if (this.summary['squat']) return this.summary['squat'];
-            else return [];
-        },
-        dips() {
-            if (this.summary['dip']) return this.summary['dip'];
-            else return [];
-        },
-        hinges() {
-            if (this.summary['hinge']) return this.summary['hinge'];
-            else return [];
-        },
-        rows() {
-            if (this.summary['row']) return this.summary['row'];
-            else return [];
-        },
-        pushups() {
-            if (this.summary['pushup']) return this.summary['pushup'];
-            else return [];
-        },
-        antiExtensions() {
-            if (this.summary['antiextension']) return this.summary['antiextension'];
-            else return [];
-        },
-        antiRotations() {
-            if (this.summary['antirotation']) return this.summary['antirotation'];
-            else return [];
-        },
-        extensions() {
-            if (this.summary['extension']) return this.summary['extension'];
-            else return [];
+        sets() {
+            let obj = {};
+            for (let ex of ['pullup', 'squat', 'dip', 'hinge', 'row', 'pushup', 'antiextension', 'antirotation', 'extension']) {
+                if (this.summary[ex]) obj[ex] = this.summary[ex];
+            }
+            return obj;
         },
     },
     data() {
@@ -166,15 +67,17 @@ export default {
             prevWorkoutId: undefined,
             workoutHistory: undefined,
             summary: JSON.parse(window.sessionStorage['workoutSummary']),
-            pullupProgression: this.$store.state.pullupProgression,
-            squatProgression: this.$store.state.squatProgression,
-            dipProgression: this.$store.state.dipProgression,
-            hingeProgression: this.$store.state.hingeProgression,
-            rowProgression: this.$store.state.rowProgression,
-            pushupProgression: this.$store.state.pushupProgression,
-            antiExtensionProgression: this.$store.state.antiExtensionProgression,
-            antiRotationProgression: this.$store.state.antiRotationProgression,
-            extensionProgression: this.$store.state.extensionProgression,
+            progressions: {
+                'pullupProgression': this.$store.state.pullupProgression,
+                'squatProgression': this.$store.state.squatProgression,
+                'dipProgression': this.$store.state.dipProgression,
+                'hingeProgression': this.$store.state.hingeProgression,
+                'rowProgression': this.$store.state.rowProgression,
+                'pushupProgression': this.$store.state.pushupProgression,
+                'antiextensionProgression': this.$store.state.antiExtensionProgression,
+                'antirotationProgression': this.$store.state.antiRotationProgression,
+                'extensionProgression': this.$store.state.extensionProgression,
+            },
         }
     }
 }
