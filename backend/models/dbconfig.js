085eb158e9b3dee423fc082960b6f4c8b2fd42c9
decaffjoe <jw3mail+decaff@pm.me>
2020-07-15 12:01:03 +1200

    Use env vars for db connection and domain

--- backend/models/dbconfig.js
+++ backend/models/dbconfig.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize').Sequelize;
 
-const db = new Sequelize('postgres://postgres:test@localhost:5432/recommended-routine', { logging: false });
+const db = new Sequelize(process.env.DB_PATH, { logging: false });
 // const db = new Sequelize('sqlite::memory:', { logging: false });
 
 // test connection
