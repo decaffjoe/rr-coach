2a108a1db91b1feeff3f630690b8ab0f31340d0a
decaffjoe <jw3mail+decaff@pm.me>
2020-07-18 11:38:51 +1200

    .env<branch> RESET_DB will reset the db

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,10 @@ app.use(express.json());
 app.use(cors());
 
 // DATABASE CONNECTION
-const { connectDB } = require('./models/seed');
-connectDB().catch(err => console.log(err));
+const { resetDB, connectDB } = require('./models/seed');
+if (process.env.RESET_DB) {
+    resetDB().catch(err => console.log(err));
+} else connectDB().catch(err => console.log(err));
 
 // RPC API ROUTES
 const userRoutes = require('./routes/user'),
