d575d4cf779ffb227889a622ed08e6115c4cd681
decaffjoe <jw3mail+decaff@pm.me>
2020-07-21 19:20:34 +1200

    Reset db

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,10 @@ app.use(cors());
 
 // DATABASE CONNECTION
 const { resetDB, connectDB } = require('./models/seed');
-if (process.env.RESET_DB) {
-    resetDB().catch(err => console.log(err));
-} else connectDB().catch(err => console.log(err));
+// if (process.env.RESET_DB) {
+//     resetDB().catch(err => console.log(err));
+// } else connectDB().catch(err => console.log(err));
+resetDB();
 
 // RPC API ROUTES
 const userRoutes = require('./routes/user'),
