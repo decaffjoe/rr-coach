9bb4070d88052fd69bdedde32e360e8f96205175
decaffjoe <jw3mail+decaff@pm.me>
2020-09-13 02:40:51 -0500

    Fix summary spacing bug when logged in

--- frontend/src/views/WorkoutSummary.vue
+++ frontend/src/views/WorkoutSummary.vue
@@ -1,31 +1,41 @@
 <template>
-    <div>
-        <Navbar />
-        <p v-if="!this.$cookies.isKey('user_id')">Want to save these stats for the future? Create an account or login from the homepage.</p>
-        <h1>Your Training Summary</h1>
-        <Summary />
-    </div>
+  <div>
+    <Navbar />
+    <p
+      v-if="!this.$cookies.isKey('user_id')"
+      :class="{ topPageMargin: !this.$cookies.isKey('user_id') }"
+    >
+      Want to save these stats for the future? Create an account or login from
+      the homepage.
+    </p>
+    <h1 :class="{ topPageMargin: this.$cookies.isKey('user_id') }">
+      Your Training Summary
+    </h1>
+    <Summary />
+  </div>
 </template>
 
 <script>
 import Navbar from "../components/Navbar.vue";
 import Summary from "../components/Summary.vue";
 export default {
-    name: "WorkoutSummary",
-    components: { Navbar, Summary, },
-}
+  name: "WorkoutSummary",
+  components: { Navbar, Summary },
+};
 </script>
 
 <style scoped>
+.topPageMargin {
+  margin-top: 59px; /* navbar spacing */
+}
 div {
-    text-align: center;
+  text-align: center;
 }
 p {
-    color: white;
-    background-color: darkolivegreen;
-    font-size: 0.9em;
-    margin: 0;
-    padding: 0.7vh 0;
-    margin-top: 59px; /* navbar spacing */
+  color: white;
+  background-color: darkolivegreen;
+  font-size: 0.9em;
+  margin: 0;
+  padding: 0.7vh 0;
 }
 </style>
